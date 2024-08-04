c3733327524f8509224facb238fca353371db66f
riskydissonance <20513519+riskydissonance@users.noreply.github.com>
2024-08-04 15:14:36 +0100

    Add raylib and basic window generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init().size(800, 450).title("Snake").build();
+
+    while !rl.window_should_close() {
+        let mut d = rl.begin_drawing(&thread);
+        d.clear_background(Color::DARKGREEN);
+        d.draw_text(
+            "This is snake, but it's not done yet.",
+            190,
+            200,
+            20,
+            Color::LIGHTGRAY,
+        );
+    }
 }
