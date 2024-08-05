a499955becfce68cc80a550123c40e6171a18467
Peter Griffiths <43973652+UnSilentOne@users.noreply.github.com>
2024-08-05 18:09:50 +0100

    feat: Align the snake and food to a grid
    Use vectors to track positions
    Define a snake speed

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
 use rand::Rng;
 use raylib::prelude::*;
+use raylib::consts::ffi::KeyboardKey;
 
 const WINDOW_WIDTH: i32 = 800;
 const WINDOW_HEIGHT: i32 = 450;
 const SNAKE_SIZE: i32 = 10;
 const FOOD_SIZE: i32 = 10;
 
+const GRID_SQUARE_SIZE: i32 = 10;
+
 // TODO
-// - Add wasm
-// - Add food
 // - Add rendering increasing length
 // - Add self-collision detection
 // - Add score
 // - Add speed increase
 // - Add cross-over sides of screen
+
+struct Vector2 {
+    x: i32,
+    y: i32
+}
+
+impl Vector2 {
+    fn new(x: i32, y: i32) -> Vector2 {
+        return Vector2{x, y};
+    }
+}
 
 enum Direction {
     Up,
@@ -23,8 +35,7 @@ enum Direction {
 }
 
 struct Food {
-    x: i32,
-    y: i32,
+    position: Vector2,
     eaten: bool,
 }
 
@@ -37,8 +48,8 @@ struct GameState {
 struct Snake {
     length: i32,
     direction: Direction,
-    x: i32,
-    y: i32,
+    position: Vector2,
+    speed: i32
 }
 
 fn main() {
@@ -51,19 +62,18 @@ fn main() {
         snake: Snake {
             length: 1,
             direction: Direction::Down,
-            x: 0,
-            y: 0,
+            position: Vector2::new(0, 0),
+            speed: GRID_SQUARE_SIZE
         },
         food: Food {
-            x: 0,
-            y: 0,
+            position: Vector2::new(0, 0),
             eaten: true,
         },
         game_over: false,
     };
 
     // TODO probably shouldn't set speed via fps
-    rl.set_target_fps(120);
+    rl.set_target_fps(10);
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
@@ -83,7 +93,7 @@ fn main() {
 
 fn is_in_snake(x: i32, y: i32, snake: &Snake) -> bool {
     // TODO box intersection
-    if x == snake.x && y == snake.y {
+    if x == snake.position.x && y == snake.position.y {
         return true;
     }
     return false;
@@ -92,24 +102,24 @@ fn is_in_snake(x: i32, y: i32, snake: &Snake) -> bool {
 fn draw_food(game_state: &mut GameState, d: &mut RaylibDrawHandle) {
     if game_state.food.eaten {
         let mut rng = rand::thread_rng();
-        let mut x = rng.gen_range(0..WINDOW_WIDTH);
-        let mut y = rng.gen_range(0..WINDOW_HEIGHT);
+        let mut x = ((rng.gen_range(0..WINDOW_WIDTH) + 5) /GRID_SQUARE_SIZE) * GRID_SQUARE_SIZE;
+        let mut y = ((rng.gen_range(0..WINDOW_HEIGHT) + 5) /GRID_SQUARE_SIZE) * GRID_SQUARE_SIZE;
         while is_in_snake(x, y, &game_state.snake) {
-            x = rng.gen_range(0..WINDOW_WIDTH);
-            y = rng.gen_range(0..WINDOW_HEIGHT);
+            x = ((rng.gen_range(0..WINDOW_WIDTH) + 5) /GRID_SQUARE_SIZE) * GRID_SQUARE_SIZE;
+            y = ((rng.gen_range(0..WINDOW_HEIGHT) + 5) / GRID_SQUARE_SIZE) * GRID_SQUARE_SIZE;
         }
-        game_state.food.x = x;
-        game_state.food.y = y;
+        game_state.food.position.x = x;
+        game_state.food.position.y = y;
         game_state.food.eaten = false;
     } else {
-        if is_in_snake(game_state.food.x, game_state.food.y, &game_state.snake) {
+        if is_in_snake(game_state.food.position.x, game_state.food.position.y, &game_state.snake) {
             game_state.snake.length += 1;
             game_state.food.eaten = true;
         }
     }
     d.draw_rectangle(
-        game_state.food.x,
-        game_state.food.y,
+        game_state.food.position.x,
+        game_state.food.position.y,
         FOOD_SIZE,
         FOOD_SIZE,
         Color::BLACK,
@@ -129,30 +139,31 @@ fn move_snake(snake: &mut Snake, d: &RaylibDrawHandle) {
     if d.is_key_pressed(KeyboardKey::KEY_RIGHT) {
         snake.direction = Direction::Right;
     }
-
+    
     match snake.direction {
         Direction::Up => {
-            snake.y -= 1;
+            snake.position.y -= snake.speed;
         }
         Direction::Down => {
-            snake.y += 1;
+            snake.position.y += snake.speed;
         }
         Direction::Left => {
-            snake.x -= 1;
+            snake.position.x -= snake.speed;
         }
         Direction::Right => {
-            snake.x += 1;
+            snake.position.x += snake.speed;
         }
     }
 }
 
 fn draw_snake(snake: &Snake, d: &mut RaylibDrawHandle) {
     // TODO draw snake body
-    d.draw_rectangle(snake.x, snake.y, SNAKE_SIZE, SNAKE_SIZE, Color::BLACK);
+
+    d.draw_rectangle(snake.position.x, snake.position.y, SNAKE_SIZE, SNAKE_SIZE, Color::BLACK);
     for i in 0..snake.length {
         d.draw_rectangle(
-            snake.x,
-            snake.y - i * SNAKE_SIZE,
+            snake.position.x,
+            snake.position.y - i * SNAKE_SIZE,
             SNAKE_SIZE,
             SNAKE_SIZE,
             Color::BLACK,
@@ -162,10 +173,10 @@ fn draw_snake(snake: &Snake, d: &mut RaylibDrawHandle) {
 
 fn check_game_over(snake: &Snake) -> bool {
     // TODO snake should actually cross over bounds and only end if intersects with self
-    if snake.x < 0
-        || snake.x + SNAKE_SIZE > WINDOW_WIDTH
-        || snake.y < 0
-        || snake.y + SNAKE_SIZE > WINDOW_HEIGHT
+    if snake.position.x < 0
+        || snake.position.x + SNAKE_SIZE > WINDOW_WIDTH
+        || snake.position.y < 0
+        || snake.position.y + SNAKE_SIZE > WINDOW_HEIGHT
     {
         return true;
     }
